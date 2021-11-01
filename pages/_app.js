2f4d343b88befffa3b62d24407fb99e0d03a93b8
dsgrillo <dsgrillo@gmail.com>
2021-11-01 16:21:59 +0100

    fix gtag

--- pages/_app.js
+++ pages/_app.js
@@ -12,6 +12,20 @@ function MyApp({ Component, pageProps }) {
           href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,700"
           rel="stylesheet"
         />
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-7W7YVP1YNE"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-7W7YVP1YNE');`,
+          }}
+        ></script>
       </Head>
       <NavHeader />
       <div className={"pt-24"}>
